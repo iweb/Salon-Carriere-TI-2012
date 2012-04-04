c41e2efced78c95cdcfd62971f1d479fdeb47720
Carl Letourneau <cletourneau@gmail.com>
2012-04-04 12:49:22 -0400

    Green, all hands kind are detected, refactored

--- pokerhands-20120402/public/PokerHand.js
+++ pokerhands-20120402/public/PokerHand.js
@@ -37,39 +37,133 @@ PokerHand.prototype.suitsValue = function(){
     return values;
 };
 
+PokerHand.prototype.rank = function() {
+    return this.findMatchingHand().label;
+};
 
-PokerHand.prototype.rank = function(){
-    if (this.isAFlush()){
-        return "Flush";
-    }
-    else if(this.isAPair()){
-        return "Pair";
-    }
-    else{
-        return this.highCard();
+PokerHand.prototype.findMatchingHand = function() {
+    for (var i = 0; i < PokerHand.highestToLowestPokerHands.length; i++) {
+        if (PokerHand.highestToLowestPokerHands[i].testFunc.apply(this)) {
+            return PokerHand.highestToLowestPokerHands[i];
+        }
     }
 };
 
-PokerHand.prototype.isAFlush = function() {
+PokerHand.prototype.isFlush = function() {
     return this.cards[0].suitValue() == this.cards[1].suitValue() &&
            this.cards[0].suitValue() == this.cards[2].suitValue() &&
            this.cards[0].suitValue() == this.cards[3].suitValue() &&
            this.cards[0].suitValue() == this.cards[4].suitValue();
-}
+};
 
-PokerHand.prototype.isAPair = function() {
-    var numberOfValues =[0,0,0,0,0,0,0,0,0,0,0,0,0];
-    for (var i = 0; i < this.cards.length; i++) {
-        numberOfValues[this.cards[i].numericalValue()]++;
-    }
-    for (var i = 0; i < numberOfValues.length; i++) {
-        if(numberOfValues[i] == 2)
-        {
-            return true;
+PokerHand.prototype.isTwoPairs = function() {
+    return this.timesItHasSpecifiedAmountOfAKind(2) == 2;
+};
+
+PokerHand.prototype.isStraightFlush = function() {
+    return this.isFlush() && this.isStraight();
+};
+
+PokerHand.prototype.isStraight = function() {
+    var numberOfValues = this.findNumberOfSameValue(), valuesInArray = [];
+    for (var i in numberOfValues) {
+        if (valuesInArray.indexOf(i) == -1) {
+            valuesInArray.push(i);
         }
     }
+    valuesInArray = valuesInArray.sort();
+    return (valuesInArray.length == 5 &&
+        (valuesInArray[valuesInArray.length-1] - valuesInArray[0]) == 4);
+};
+
+PokerHand.prototype.isFourOfAKind = function() {
+    return this.hasSpecifiedAmountOfAKind(4);
+};
+
+PokerHand.prototype.isFullHouse = function() {
+    return this.isPair() && this.isThreeOfAKind();
+};
+
+PokerHand.prototype.isThreeOfAKind = function() {
+    return this.hasSpecifiedAmountOfAKind(3);
+};
+
+PokerHand.prototype.isPair = function() {
+    return this.hasSpecifiedAmountOfAKind(2);
+};
+
+PokerHand.prototype.isHighCard = function() {
+    return true;
+};
+
+PokerHand.prototype.findNumberOfSameValue = function() {
+    var numberOfValues = {};
+    for (var i = 0; i < this.cards.length; i++) {
+        var key = this.cards[i].numericalValue().toString();
+        if(!numberOfValues[key])
+        {
+            numberOfValues[key] = 1;
+        }
+        else
+        {
+            numberOfValues[key]++;
+        }
+    }
+    return numberOfValues;
+};
+
+PokerHand.prototype.hasSpecifiedAmountOfAKind = function(amount) {
+    return this.timesItHasSpecifiedAmountOfAKind(amount) == 1;
+};
+
+PokerHand.prototype.timesItHasSpecifiedAmountOfAKind = function(amount) {
+    var numberOfValues = this.findNumberOfSameValue();
+    var numberOfSameValueGroup = 0;
+    for (var key in numberOfValues) {
+        if(numberOfValues[key] == amount)
+        {
+            numberOfSameValueGroup++;
+        }
+    }
+    return numberOfSameValueGroup;
 }
 
-PokerHand.prototype.highCard = function() {
-    return "High Card";
-}
+
+PokerHand.highestToLowestPokerHands = [
+    {
+        label:"Straight Flush",
+        testFunc:PokerHand.prototype.isStraightFlush
+    },
+    {
+        label:"Four of a Kind",
+        testFunc:PokerHand.prototype.isFourOfAKind
+    },
+    {
+        label:"Full House",
+        testFunc:PokerHand.prototype.isFullHouse
+    },
+    {
+        label:"Flush",
+        testFunc:PokerHand.prototype.isFlush
+    },
+    {
+        label:"Straight",
+        testFunc:PokerHand.prototype.isStraight
+    },
+    {
+        label:"Three of a Kind",
+        testFunc:PokerHand.prototype.isThreeOfAKind
+    },
+    {
+        label:"Two Pairs",
+        testFunc:PokerHand.prototype.isTwoPairs
+    },
+    {
+        label:"Pair",
+        testFunc:PokerHand.prototype.isPair
+    },
+    {
+        label:"High Card",
+        testFunc:PokerHand.prototype.isHighCard
+    }
+];
--- pokerhands-20120402/spec/PokerHandsSpec.js
+++ pokerhands-20120402/spec/PokerHandsSpec.js
@@ -30,6 +30,36 @@ describe("PokerHand", function(){
             var pokerHand = new PokerHand("2C 2D AC 8S 7C");
             expect(pokerHand.rank()).toEqual("Pair");
         });
+
+        it("two pairs", function () {
+            var pokerHand = new PokerHand("2C 2D AC AS 7C");
+            expect(pokerHand.rank()).toEqual("Two Pairs");
+        });
+
+        it("three of a kind", function () {
+            var pokerHand = new PokerHand("2C AD AC AS 7C");
+            expect(pokerHand.rank()).toEqual("Three of a Kind");
+        });
+
+        it("full house", function () {
+            var pokerHand = new PokerHand("3C AD AC AS 3C");
+            expect(pokerHand.rank()).toEqual("Full House");
+        });
+
+        it("four of a kind", function () {
+            var pokerHand = new PokerHand("5C 5D 5H 5S 3C");
+            expect(pokerHand.rank()).toEqual("Four of a Kind");
+        });
+
+        it("straight", function () {
+            var pokerHand = new PokerHand("3C 4D 5H 6S 7C");
+            expect(pokerHand.rank()).toEqual("Straight");
+        });
+
+        it("straight flush", function () {
+            var pokerHand = new PokerHand("3C 4C 5C 6C 7C");
+            expect(pokerHand.rank()).toEqual("Straight Flush");
+        });
     });
 });
 
